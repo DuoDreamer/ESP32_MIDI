d96dc6805ebd8d8f3730542c0eb161d2ce0884e1
Duo <rownik@hotmail.com>
2025-08-06 18:37:21 +0000

    Initialize ESP-IDF project with custom partitions

---
+++ main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+
+static const char *TAG = "ESP32_MIDI";
+
+void app_main(void)
+{
+    ESP_LOGI(TAG, "ESP32 MIDI project initialized");
+}
